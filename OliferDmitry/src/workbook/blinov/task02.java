f5c3c5cc203f56c4419e9e8280abff1e7f729451
DemonWTF <6292801@gmail.com>
2015-02-20 22:05:00 +0300

    blinov - task02

--- OliferDmitry/src/workbook/blinov/task02.java
+++ OliferDmitry/src/workbook/blinov/task02.java
@@ -3,7 +3,7 @@ package workbook.blinov;
 
 public class task02 {
     public static void main(String[] args) {
-        System.out.println("khfgl");
+        System.out.println("khfglkjgjhgj");
 
     }
 }
