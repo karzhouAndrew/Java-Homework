d19c70accbe74d02ddf741914214adf22d4dfb4c
alexeyandreenko <alexey.andreenko@gmail.com>
2015-02-20 23:01:12 +0300

    Задача №1

---
+++ andreenko/src/homework/task_1/HelloWorld.java
@@ -0,0 +1,10 @@
+package homework.task_1;
+
+/**
+ * Created by Алексей Андреенко on 20.02.2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
