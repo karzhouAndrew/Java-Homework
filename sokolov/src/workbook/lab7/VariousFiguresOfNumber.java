6268bbd7afd4780f7c080082bfbc39f7946e11bd
Denis <j.dp@yandex.by>
2015-02-23 09:51:02 +0300

    DSOKOLOV / labs # 1-21 / correction labs 6 and 7

--- sokolov/src/workbook/lab7/VariousFiguresOfNumber.java
+++ sokolov/src/workbook/lab7/VariousFiguresOfNumber.java
@@ -2,11 +2,11 @@ package workbook.lab7;
 
 public class VariousFiguresOfNumber {
     public static void main(String[] args) {
-        int a = 6321;
-        int one = a % 10;
-        int two = (a % 100 - one) / 10;
-        int three = (a % 1000 - one - two * 10) / 10;
-        int four = (a - one - two * 10 - three * 100) / 1000;
+        int number = 6321;
+        int one = number % 10;
+        int two = (number % 100 - one) / 10;
+        int three = (number % 1000 - one - two * 10) / 10;
+        int four = (number - one - two * 10 - three * 100) / 1000;
         if ((one != two) && (one != three) && (one != four)) {
             if ((two != three) && (two != four) && (three != four)) {
                 System.out.println("true");
--- sokolov/src/workbook/lab8/Factorial.java
+++ sokolov/src/workbook/lab8/Factorial.java
@@ -1,10 +1,11 @@
 package workbook.lab8;
 
 public class Factorial {
-    public static void main(String[] argv) {
+    public static void main(String[] args) {
         int number = 10, factorial = 1;
-        for (int i = 1; i <= number; i++)
+        for (int i = 1; i <= number; i++) {
             factorial *= i;
+        }
         System.out.println("the factorial of " + number + " is equal " + factorial);
     }
 }
