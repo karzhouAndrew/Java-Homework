2181ff7a57775e8fe3dd6dd3a7d1ced41333fa8a
DemonWTF <6292801@gmail.com>
2015-02-27 00:35:25 +0300

    annotation task

---
+++ OliferDmitry/src/workbook/annotationTask/Main.java
@@ -0,0 +1,12 @@
+package workbook.annotationTask;
+
+import static workbook.annotationTask.Methods.*;
+
+public class Main {
+    public static void main(String[] args) {
+        method1();
+        method2();
+
+
+    }
+}
