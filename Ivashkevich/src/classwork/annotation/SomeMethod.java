3e7155f86a2ab887fcaafa02ffe58c1cc4a4fcd7
leonevich <leonevich@gmail.com>
2015-02-26 23:15:05 +0300

    src

---
+++ Ivashkevich/src/classwork/annotation/SomeMethod.java
@@ -0,0 +1,17 @@
+package classwork.annotation;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by Leonid on 26.02.2015.
+ */
+public class SomeMethod {
+    @Transaction
+    public void method (){
+
+    }
+}
+
+
+
