99225b3dd21deb0b291203573b7f5def0b855b32
Eugene Markovich <4funby@gmail.com>
2015-03-04 02:19:57 +0300

    Workbook Lab # 28 ver. 1.0

---
+++ markovich/src/workbook/lab28/Main.java
@@ -0,0 +1,11 @@
+package workbook.lab28;
+
+/**
+ * Создать список оценок учеников с помощью ArrayList, заполнить случайными оценками. Найти самую высокую оценку с использованием итератора.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
