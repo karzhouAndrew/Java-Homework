7a4f6dbeab6d54e9ff86d564c4e939eba12b286a
alexeyandreenko <alexey.andreenko@gmail.com>
2015-03-05 22:13:22 +0300

    Задача №16

--- andreenko/src/homework/task_1/task_16/TestAllPunct.java
+++ andreenko/src/homework/task_1/task_16/TestAllPunct.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 /**
  * Created by Алексей Андреенко on 17.02.2015.
- * Найти в строке не только запятые, но и другие знаки препинания. Подсчитать их общее количество.
+ * Найти в строке не только запятые, но и другие знаки препинания. Подсчитать их общее количество!
  */
 public class TestAllPunct {
     public static void main(String[] args) throws IOException {
