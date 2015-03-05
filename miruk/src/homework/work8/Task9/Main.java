db24c3f66d701a1fb1907d88df27011d90408263
Tonya <Sidtonya@yandex.ru>
2015-03-05 23:23:49 +0300

    Task9

---
+++ miruk/src/homework/work8/Task9/Main.java
@@ -0,0 +1,10 @@
+package homework.work8.Task9;
+
+/**
+ * Создать список оценок учеников с помощью ArryList, заполнить случайными оценками. Найти самую высокую оценку с использованием итератора.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
