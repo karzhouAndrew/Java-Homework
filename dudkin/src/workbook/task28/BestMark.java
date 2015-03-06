df862cf52c178e1e9bd06c3f01be197de34c9bee
dudkinkonstantin <dudkinkonstantin@gmail.com>
2015-03-06 21:25:43 +0300

    Коллекции

---
+++ dudkin/src/workbook/task28/BestMark.java
@@ -0,0 +1,21 @@
+package workbook.task28;
+
+import java.util.*;
+
+public class BestMark {
+    public static void main(String[] args) {
+        Object mark = null;
+        List<Integer> arrayList = new ArrayList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            arrayList.add((int) (Math.random() * 10 + 1));
+        }
+        System.out.println("Список оценок: " + arrayList);
+        Set<Integer> bestMark = new TreeSet<Integer>(arrayList);
+        Iterator iterator = bestMark.iterator();
+        while (iterator.hasNext()) {
+            mark = iterator.next();
+        }
+        System.out.println("Самая высокая оценка: " + mark);
+    }
+
+}
