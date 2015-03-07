e0728451c9f3f2c1873650b78af7b4cab86b76b9
Eugene Markovich <4funby@gmail.com>
2015-03-07 03:19:24 +0300

    Classes Lab#10.2 ver 1.1

--- markovich/src/classes/lab10/lab10_2/Main.java
+++ markovich/src/classes/lab10/lab10_2/Main.java
@@ -1,14 +1,20 @@
 package classes.lab10.lab10_2;
 
+import java.util.Arrays;
+
 /**
  * Создайте итератор, который может проходить массив строк.
  */
 public class Main {
     public static void main(String[] args) {
-        String[] words = new String[]{"mather","father","son","daughter","sister","brother","grandfather","grandmother","uncle"};
 
-        for(String word : words) {
-            System.out.println(word);
+        String[] words = {"mather","father","son","daughter","sister","brother","grandfather","grandmother","uncle"};
+
+        StringIterator stringIterator = new StringIterator(words);
+
+        while (stringIterator.hasNext()) {
+            String str = stringIterator.next();
+            System.out.println(str);
         }
     }
 }
--- markovich/src/classes/lab10/lab10_2/StringIterator.java
+++ markovich/src/classes/lab10/lab10_2/StringIterator.java
@@ -2,24 +2,31 @@ package classes.lab10.lab10_2;
 
 import java.util.Iterator;
 
-public class StringIterator<Type> implements Iterator<Type> {
-    private Type[] array;
-    private int currentSize;
-    private int currentIndex = 0;
+public class StringIterator implements Iterator{
+    private String[] stringArray;
+    private int stringIndex;
 
-    public StringIterator(Type[] array) {
-        this.array = array;
-        this.currentSize = currentSize;
+    public StringIterator(String[] strArray) {
+        this.stringArray = strArray;
+        this.stringIndex = 0;
     }
 
     @Override
     public boolean hasNext() {
-        return currentIndex < currentSize && array[currentIndex] != null;
+        if (stringIndex < stringArray.length) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
-    public Type next() {
-        return array[currentIndex++];
+    public String next() {
+        if (stringIndex < stringArray.length) {
+            return stringArray[stringIndex++];
+        } else {
+            return null;
+        }
     }
 
     @Override
