7292b4eea6d2b77902c9728b87a78a3d466daba1
Eugene Markovich <4funby@gmail.com>
2015-03-07 15:08:11 +0300

    Workbook Lab #29 ver. 1.0

---
+++ markovich/src/workbook/lab29/Main.java
@@ -0,0 +1,14 @@
+package workbook.lab29;
+
+/**
+ * Частотный словарь
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "Я изучаю мапы Я вопросов и у e e нет у нет уменя нет вопросов";
+        System.out.println(Word.wordsCounter(str));
+    }
+}
+
