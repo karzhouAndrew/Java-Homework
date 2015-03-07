09c0facc4318db1410062bd558bb6c070e1037b2
Eugene Markovich <4funby@gmail.com>
2015-03-07 03:19:55 +0300

    Classes Lab#10.2 ver 1.1

--- markovich/src/classes/lab10/lab10_2/Main.java
+++ markovich/src/classes/lab10/lab10_2/Main.java
@@ -1,14 +1,12 @@
 package classes.lab10.lab10_2;
 
-import java.util.Arrays;
-
 /**
  * Создайте итератор, который может проходить массив строк.
  */
 public class Main {
     public static void main(String[] args) {
 
-        String[] words = {"mather","father","son","daughter","sister","brother","grandfather","grandmother","uncle"};
+        String[] words = {"mather", "father", "son", "daughter", "sister", "brother", "grandfather", "grandmother", "uncle"};
 
         StringIterator stringIterator = new StringIterator(words);
 
