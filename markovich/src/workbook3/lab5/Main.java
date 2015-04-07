975303a85a75ca9d122711aeb6c574ffd034588e
Eugene <4funby@gmail.com>
2015-04-07 15:27:28 +0300

    Workbook 3 Lab 5 ver 1.1

--- markovich/src/workbook3/lab5/Main.java
+++ markovich/src/workbook3/lab5/Main.java
@@ -14,36 +14,32 @@ public class Main {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "admin";
 
+    private static final String SHOW_TABLE_QUERY = "SELECT paydate, value, nameReceiver FROM expenses,receivers WHERE receiver=receivers.idReceiver";
+
+    private static final String INTO_QUERY_1 = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-01', '1', '17300')";
+    private static final String INTO_QUERY_2 = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-02', '2', '35900')";
+    private static final String INTO_QUERY_3 = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-03', '3', '80500')";
+    private static final String INTO_QUERY_4 = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-04', '1', '14200')";
+    private static final String INTO_QUERY_5 = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-05', '2', '63000')";
+
     private static Connection conn;
     private static Statement statement;
+    private static ResultSet result;
 
     public static void main(String[] args) {
 
         try {
             Class.forName(JDBC_DRIVER);
-            System.out.println("Connection is OK!");
-
-            String showTableQuery = "SELECT paydate, value, nameReceiver FROM expenses,receivers WHERE receiver=receivers.idReceiver";
 
             conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             statement = conn.createStatement();
+            statement.executeUpdate(INTO_QUERY_1);
+            statement.executeUpdate(INTO_QUERY_2);
+            statement.executeUpdate(INTO_QUERY_3);
+            statement.executeUpdate(INTO_QUERY_4);
+            statement.executeUpdate(INTO_QUERY_5);
 
-            String intoQuery = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-01', '1', '17300')";
-            statement.executeUpdate(intoQuery);
-
-            intoQuery = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-02', '2', '35900')";
-            statement.executeUpdate(intoQuery);
-
-            intoQuery = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-03', '3', '80500')";
-            statement.executeUpdate(intoQuery);
-
-            intoQuery = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-04', '1', '14200')";
-            statement.executeUpdate(intoQuery);
-
-            intoQuery = "INSERT INTO expenses (paydate, receiver, value) VALUES('2015-04-05', '2', '63000')";
-            statement.executeUpdate(intoQuery);
-
-            ResultSet result = statement.executeQuery(showTableQuery);
+            result = statement.executeQuery(SHOW_TABLE_QUERY);
 
             while (result.next()) {
                 System.out.println(result.getDate(1) + " " + result.getInt(2) + " " + result.getString(3));
@@ -57,6 +53,12 @@ public class Main {
             try {
                 if (conn != null) {
                     conn.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (result != null) {
+                    result.close();
                 }
             } catch (SQLException se) {
                 se.printStackTrace();
