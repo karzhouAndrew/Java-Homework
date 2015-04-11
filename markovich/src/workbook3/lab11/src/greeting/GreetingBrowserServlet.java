386f2fa9d3c3b5e4c2b799147b4126b38a531314
Eugene <4funby@gmail.com>
2015-04-11 20:31:19 +0300

    Workbook 3 Lab 11 ver 1.0

---
+++ markovich/src/workbook3/lab11/src/greeting/GreetingBrowserServlet.java
@@ -0,0 +1,44 @@
+package greeting;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Сделать сервлет, который по содержимому User-Agent будет определять
+ * с помощью какого браузера зашел пользователь, и выводить сообщение вида: Приветствую пользователя Firefox
+ */
+
+@WebServlet(name = "GreetingBrowserServlet")
+public class GreetingBrowserServlet extends HttpServlet {
+    private static String headTemplate = "<html><head><title>Servlet Example</title></head>";
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String userAgent = request.getHeader("user-agent");
+
+        PrintWriter out = response.getWriter();
+        out.println(headTemplate);
+        out.println("<body><div align='center' style='margin-top: 300px;'>");
+        if (userAgent.contains("MSIE") || userAgent.contains("IE")) {
+            out.println("Welcome IE user" + "<br>");
+        }
+        if (userAgent.contains("Safari")) {
+            out.println("Welcome Safari user" + "<br>");
+        }
+        if (userAgent.contains("Opera") || userAgent.contains("OPR")) {
+            out.println("Welcome Opera user" + "<br>");
+        }
+        if (userAgent.contains("Chrome")) {
+            out.println("Welcome Google Chrome user" + "<br>");
+        }
+        if (userAgent.contains("Firefox")) {
+            out.println("Welcome Mozilla Firefox user" + "<br>");
+        }
+        out.println("</div></body></html>");
+    }
+}
