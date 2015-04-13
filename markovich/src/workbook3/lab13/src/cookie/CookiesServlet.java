c4237521b898ad9d0da0a4f78ea3acf8aa240893
Eugene <4funby@gmail.com>
2015-04-14 01:40:05 +0300

    Workbook 3 Lab 13 ver 1.1

--- markovich/src/workbook3/lab13/src/cookie/CookiesServlet.java
+++ markovich/src/workbook3/lab13/src/cookie/CookiesServlet.java
@@ -15,41 +15,49 @@ import java.io.PrintWriter;
 
 public class CookiesServlet extends HttpServlet {
     private int counter;
+    private static String cookieValue = null;
+    private static String cookieName = "uid";
 
     @Override
     public void init() throws ServletException {
         counter = 0;
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
         out.println("<html><head><title>Cookies Demo</title></head>");
         out.println("<body>");
 
-        Cookie[] myCookies = request.getCookies();
-        String cookieName = "uid";
-        String cookieValue = null;
-        for (int i = 0; i < myCookies.length; i++) {
-            Cookie cookie = myCookies[i];
-            if (cookieName.equals(cookie.getName())) {
-                cookieValue = cookie.getValue();
-                break;
-            }
-        }
-        if (cookieValue==null){
-            Cookie myCookie = new Cookie("uid", "92cd270e-5ef6-4903-a704-3af8dafabdb8");
-            myCookie.setMaxAge(5);
-            response.addCookie(myCookie);
-            counter++;
-        }
+        cookieValue = getCookieValue(request);
+
+        checkCookie(cookieValue, response);
 
         out.println("<p align = center> Cookie value - " + cookieValue + "</p>");
         out.println("<p align = center> Total counter - " + counter + "</p>");
         out.println("</body></html>");
         out.close();
+    }
 
+    private String getCookieValue(HttpServletRequest request) {
+        Cookie[] myCookies = request.getCookies();
+        String strValue = null;
+        for (int i = 0; i < myCookies.length; i++) {
+            Cookie cookie = myCookies[i];
+            if (cookieName.equals(cookie.getName())) {
+                strValue = cookie.getValue();
+                break;
+            }
+        }
+        return strValue;
+    }
+
+    private void checkCookie(String strValue, HttpServletResponse response) {
+        if (cookieValue == null) {
+            Cookie myCookie = new Cookie("uid", "92cd270e-5ef6-4903-a704-3af8dafabdb8");
+            myCookie.setMaxAge(5);
+            response.addCookie(myCookie);
+            counter++;
+        }
     }
 }
